f138fdaa85253a349e7d83490fd1f634b993972b
Varsha-Nayak04 <kvarshanayak@gmail.com>
2025-05-28 11:33:01 +0530

    Add files via upload

---
+++ maventest101/src/main/java/com/program2/maven/program2/App.java
@@ -0,0 +1,10 @@
+package com.program2.maven.program2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
